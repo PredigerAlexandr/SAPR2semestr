708b2c7a1228aea073f05036b7d62cbabef3e2e9
Alexandr <Alexandr@DESKTOP-QQCGJ2F>
2023-02-22 01:54:03 +0400

    54654544kiii

---
+++ SAPR/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SAPR.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SAPR.Controllers
+{
+    public class AdminController : Controller
+    {
+
+        DataBaseContext db;
+        public AdminController(DataBaseContext context)
+        {
+            db = context;
+        }
+
+        [HttpGet]
+        public IActionResult AdminRules()
+        {
+            ViewBag.Purchases = db.Purchases;
+            return View();
+        }
+    }
+}
